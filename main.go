eab6d4f383654135c0f043fdea0944c2488322ac
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2021-06-15 14:07:13 +1000

    Basic configurable serial port sniffer

--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
-import ()
+import (
+  "fmt"
+  "log"
+  "flag"
+  "github.com/tarm/serial"
+)
 
-var ()
+var (
+  targetComPort string
+  baudRate int
+)
+
+// init flags
+func init() {
+	flag.StringVar(&targetComPort, "target", "/dev/ttyAMA0", "specify com port to sniff")
+	flag.IntVar(&baudRate, "baud", 9600, "specify baud rate")
+}
 
 func main() {
-  // Hello, world!
+  config := &serial.Config{
+    Name: targetComPort,
+    Baud: baudRate,
+    ReadTimeout: 1,
+    Size 8
+  }
+  
+  stream, err := serial.OpenPort(config)
+  if err != nil {
+    log.Fatal(err)
+  }
+  buf := make([]byte, 1024)
+
+  for {
+    n, err := stream.Read(buf)
+    if err != nil {
+            log.Fatal(err)
+    }
+    s := string(buf[:n])
+    fmt.Println(s) // Replace with multi logger (stdout & text file)
+  }
 }
